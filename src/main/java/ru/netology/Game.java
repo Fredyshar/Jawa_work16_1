f824b021063dddbf8ae4a4f2db943aa021ae51b0
Fedor Sharov <1710193@mail.ru>
2022-10-08 10:07:06 +0300

    Branch coverage 100%

---
+++ src/main/java/ru/netology/Game.java
@@ -0,0 +1,57 @@
+package ru.netology;
+
+
+import java.util.ArrayList;
+
+public class Game {
+
+    ArrayList<Player> players = new ArrayList<>();
+
+    public void register(Player player) {
+        //реализация метода регистрации
+        players.add(player);
+    }
+
+    public byte round(String playerName1, String playerName2) {
+        // написать метод соревнования м/у двумя игроками
+        Player player1 = null;
+        Player player2 = null;
+        for (Player player : players) {
+            if (player.getName().equals(playerName1)) {
+                player1 = player;
+            }
+            if (player.getName().equals(playerName2)) {
+                player2 = player;
+            }
+        }
+        if (player1 == null && player2 == null) {
+            throw new NotRegisteredException(
+                    "Player with name: " + playerName1 + " not registered "
+                    + " and " +
+                    "Player with name: " + playerName2 + " not registered "
+            );
+        }
+        if (player1 == null) {
+            throw new NotRegisteredException(
+                    "Player with name: " + playerName1 + " not registered "
+            );
+        }
+        if (player2 == null) {
+            throw new NotRegisteredException(
+                    "Player with name: " + playerName2 + " not registered "
+            );
+        }
+        if (player1.getStrength() > player2.getStrength()) {
+            return 1;
+        }
+        if (player1.getStrength() < player2.getStrength()) {
+            return 2;
+        }
+        return 0;
+
+    }
+
+}
+
+
+
---
+++ src/test/java/ru/netology/GameTest.java
@@ -0,0 +1,100 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class GameTest {
+
+    Player player1 = new Player(1, "Batman", 20);
+    Player player2 = new Player(2, "Superman", 20);
+    Player player3 = new Player(3, "Robin", 15);
+    Player player4 = new Player(4, "Joker", 40);
+
+
+    @Test
+    public void allPlayersRegistered() {                    //все икроки зарегестрированы
+
+        Game game = new Game();
+        game.register(player1);
+        game.register(player2);
+
+        byte actual = game.round("Batman", "Superman");
+        byte expected = 0;
+        Assertions.assertEquals(actual, expected);
+    }
+    @Test
+    public void firstPlayerNotRegistered() {                  //первый игрок не зарегестрирован
+        Game game = new Game();
+        game.register(player1);
+        game.register(player2);
+
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            game.round("Joker", "Superman");
+        });
+//        byte actual = game.round("Superman", "Joker");
+//        System.out.println(actual);
+    }
+    @Test
+    public void secondPlayerNotRegistered() {                  //второй игрок не зарегестрирован
+        Game game = new Game();
+        game.register(player1);
+        game.register(player2);
+
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            game.round("Superman", "Joker");
+        });
+//        byte actual = game.round("Superman", "Joker");
+//        System.out.println(actual);
+    }
+    @Test
+    public void twoPlayersNotRegistered() {                  //два игрока не зарегестрированы
+        Game game = new Game();
+        game.register(player1);
+        game.register(player2);
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            game.round("Robin", "Joker");
+        });
+//        byte actual = game.round("Robin", "Joker");
+//        System.out.println(actual);
+    }
+    @Test
+    public void strengthPlayersEqual() {                 //силы игроков равны
+        Game game = new Game();
+        game.register(player1);
+        game.register(player2);
+
+        byte expected = 0;
+        byte actual = game.round("Batman", "Superman");
+
+        Assertions.assertEquals(actual, expected);
+    }
+    @Test
+    public void strengthPlayer1More() {                 //силы игрока 1 больше
+        Game game = new Game();
+        game.register(player1);
+        game.register(player3);
+
+        byte expected = 1;
+        byte actual = game.round("Batman", "Robin");
+
+        Assertions.assertEquals(actual, expected);
+    }
+    @Test
+    public void strengthPlayer2More() {                 //силы игрока 2 больше
+        Game game = new Game();
+        game.register(player1);
+        game.register(player4);
+
+        byte expected = 2;
+        byte actual = game.round("Batman", "Joker");
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+}
