84d874468eba1ee1391a2210b98735137f3b87af
Fedor Sharov <1710193@mail.ru>
2022-10-21 07:30:34 +0300

    Вeтка Fast- Изменил покрытие по бранчам. Покрытие 100%

--- src/main/java/ru/netology/NotRegisteredException.java
+++ src/main/java/ru/netology/NotRegisteredException.java
@@ -1,6 +1,6 @@
 package ru.netology;
 
-public class NotRegisteredException extends RuntimeException{
+public class NotRegisteredException extends RuntimeException {
     public NotRegisteredException(String msg) {
         super(msg);
     }
